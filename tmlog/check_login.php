5757de52fc85e14ad71e4512eff60d781693939b
zhaokang <zhaokang19950802@163.com>
2017-02-17 11:19:34 +0800

    first commit

---
+++ tmlog/check_login.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL || ~E_NOTICE);
+session_start();
+if($_SESSION['username']==""){
+	echo "<script>alert('对不起，本博客网站需要进行用户登录来验证您的真实身份!');window.location.href='index.php';</script>";
+	exit();
+}
+?>
---
+++ tmlog/del_comment.php
@@ -0,0 +1,13 @@
+<?php
+	session_start();
+	include "Conn/conn.php";
+	$comment_id=$_GET['comment_id'];
+    $sql="delete from tb_filecomment where id=".$comment_id;
+    $result=mysql_query($sql);
+	if($result){
+		echo "<script>alert('É¾³ý³É¹¦!');location='$_SERVER[HTTP_REFERER]';</script>";
+	}
+	else{
+		echo "<script>alert('É¾³ý²Ù×÷Ê§°Ü!');history.go(-1);</script>";
+	}
+?>
